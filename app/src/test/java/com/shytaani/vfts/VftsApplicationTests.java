0ab98c0ab589912d39ea1d7aa628380b424848fc
Tanishy <mambaout134@gmail.com>
2022-07-23 16:54:59 +0900

    Create spring boot application template

---
+++ app/src/test/java/com/shytaani/vfts/VftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shytaani.vfts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
