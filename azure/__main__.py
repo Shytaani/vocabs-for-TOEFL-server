0ca96004d0a773d408650d8a87f6bc3f40a14f9c
Tanishy <mambaout134@gmail.com>
2022-11-30 02:04:38 +0900

    Replace resource to deploy container from ACI to App Service

--- azure/__main__.py
+++ azure/__main__.py
@@ -2,7 +2,8 @@
 
 import pulumi
 import pulumi_docker as docker
-from pulumi_azure_native import containerinstance
+# from pulumi_azure_native import containerinstance
+from pulumi_azure_native import web
 from pulumi_azure_native import containerregistry
 from pulumi_azure_native import resources
 
@@ -33,7 +34,9 @@ docker_image = docker.Image(
     custom_image,
     image_name=registry.login_server.apply(
         lambda login_server: f"{login_server}/{custom_image}:v1.0.0"),
-    build=docker.DockerBuild(context=f"./../{custom_image}"),
+    build=docker.DockerBuild(
+        context=f"./../{custom_image}",
+        extra_options=["--platform", "linux/amd64"]),
     registry=docker.ImageRegistry(
         server=registry.login_server,
         username=admin_username,
@@ -42,28 +45,67 @@ docker_image = docker.Image(
 
 pulumi.export('image_name', docker_image.image_name)
 
+# Create App Service Plan
+plan = web.AppServicePlan(
+    "vftsPlan",
+    name="vftsPlan",
+    resource_group_name=resource_group.name,
+    kind="Linux",
+    reserved=True,
+    sku=web.SkuDescriptionArgs(
+        name="B1",
+        tier="Basic",
+    )
+)
+# Run a container on App Service
+app = web.WebApp(
+    "vfts-app",
+    name="vocabs-for-TOEFL",
+    resource_group_name=resource_group.name,
+    server_farm_id=plan.id,
+    site_config=web.SiteConfigArgs(
+        app_settings=[
+            web.NameValuePairArgs(name="WEBSITES_ENABLE_APP_SERVICE_STORAGE", value="false"),
+            web.NameValuePairArgs(name="DOCKER_REGISTRY_SERVER_URL",
+                                  value=registry.login_server.apply(
+                                      lambda login_server: f"https://{login_server}")),
+            web.NameValuePairArgs(name="DOCKER_REGISTRY_SERVER_USERNAME",
+                                  value=admin_username),
+            web.NameValuePairArgs(name="DOCKER_REGISTRY_SERVER_PASSWORD",
+                                  value=admin_password),
+            web.NameValuePairArgs(name="WEBSITES_PORT", value="8080"),
+        ],
+        always_on=True,
+        linux_fx_version=docker_image.image_name.apply(lambda image_name: f"DOCKER|{image_name}"),
+    ),
+    https_only=True)
+
+pulumi.export(
+    "appEndpoint",
+    app.default_host_name.apply(lambda default_host_name: f"https://{default_host_name}"))
+
 # Create an Azure Container Instance
-container_group = containerinstance.ContainerGroup("VFTS-CG",
-    container_group_name="VFTS-CG",
-    containers=[containerinstance.ContainerArgs(
-        command=[],
-        environment_variables=[],
-        image=docker_image.image_name,
-        name="vfts",
-        ports=[containerinstance.ContainerPortArgs(
-            port=80,
-        )],
-        resources=containerinstance.ResourceRequirementsArgs(
-            requests=containerinstance.ResourceRequestsArgs(
-                cpu=1,
-                memory_in_gb=1.5,
-            ),
-        ),
-    )],
-    image_registry_credentials=[containerinstance.ImageRegistryCredentialArgs(
-            server=registry.login_server,
-            username=admin_username,
-            password=admin_password
-    )],
-    os_type="Linux",
-    resource_group_name=resource_group.name)
+# container_group = containerinstance.ContainerGroup("VFTS-CG",
+#     container_group_name="VFTS-CG",
+#     containers=[containerinstance.ContainerArgs(
+#         command=[],
+#         environment_variables=[],
+#         image=docker_image.image_name,
+#         name="vfts",
+#         ports=[containerinstance.ContainerPortArgs(
+#             port=80,
+#         )],
+#         resources=containerinstance.ResourceRequirementsArgs(
+#             requests=containerinstance.ResourceRequestsArgs(
+#                 cpu=1,
+#                 memory_in_gb=1.5,
+#             ),
+#         ),
+#     )],
+#     image_registry_credentials=[containerinstance.ImageRegistryCredentialArgs(
+#             server=registry.login_server,
+#             username=admin_username,
+#             password=admin_password
+#     )],
+#     os_type="Linux",
+#     resource_group_name=resource_group.name)
