ebbbe950943ff2a85937e093a669ef14207587ee
Tanishy <mambaout134@gmail.com>
2022-12-30 01:07:51 +0900

    Modify AWS resource definitions(HTTP->HTTPS, Refactor)

--- aws/__main__.py
+++ aws/__main__.py
@@ -3,25 +3,81 @@ import pulumi_aws as aws
 import pulumi_awsx as awsx
 
 config = pulumi.Config()
+site_domain_name = config.get("siteDomainName")
 container_port = config.get_int("containerPort", 80)
 cpu = config.get_int("cpu", 512)
 memory = config.get_int("memory", 128)
 
 # A custom VPC contains resources
-vpc = awsx.ec2.Vpc("vftsVPC")
+vpc = awsx.ec2.Vpc("vftsVPC",
+    availability_zone_names=[
+        "ap-northeast-1a",
+        "ap-northeast-1c"])
 pulumi.export("vpcId", vpc.vpc_id)
 pulumi.export("publicSubnetIds", vpc.public_subnet_ids)
 pulumi.export("privateSubnetIds", vpc.private_subnet_ids)
 
 # An ECS cluster to deploy into
-cluster = aws.ecs.Cluster("vftsCluster",
-    name="vftsCluster")
+cluster = aws.ecs.Cluster("vftsCluster", name="vftsCluster")
+
+# An ACM Certificate for the domain
+certificate = aws.acm.Certificate("vftsCert",
+    domain_name=site_domain_name,
+    validation_method="DNS",
+    opts=pulumi.ResourceOptions(
+        delete_before_replace=True))
 
 # An ALB to serve the container endpoint to the internet
 loadbalancer = awsx.lb.ApplicationLoadBalancer("vftsLB",
-    awsx.lb.ApplicationLoadBalancerArgs(
-        name="vftsLB",
-        subnet_ids=vpc.public_subnet_ids))
+    name="vftsLB",
+    subnet_ids=vpc.public_subnet_ids)
+
+# A Route53 public host zone
+zone = aws.route53.Zone("vftsZone", name=certificate.domain_name)
+
+# A DNS record to route traffic from the domain to a load balancer
+loadbalancer_alias_record = aws.route53.Record("vftsLBAliasRecord",
+    zone_id=zone.zone_id,
+    name=certificate.domain_name,
+    type="A",
+    aliases=[aws.route53.RecordAliasArgs(
+        name=loadbalancer.load_balancer.dns_name,
+        zone_id=loadbalancer.load_balancer.zone_id,
+        evaluate_target_health=True)])
+pulumi.export("vftsLBAliasRecordFqdn", loadbalancer_alias_record.fqdn)
+
+# A DNS record of a certificate's CNAME record
+certificate_record = aws.route53.Record("vftsCertRecord",
+    name=certificate.domain_validation_options[0].resource_record_name,
+    records=[certificate.domain_validation_options[0].resource_record_value],
+    ttl=60,
+    type=certificate.domain_validation_options[0].resource_record_type,
+    zone_id=zone.zone_id)
+
+# A resource to run certificate validation
+certificate_validation = aws.acm.CertificateValidation("vftsCertValidation",
+    certificate_arn=certificate.arn,
+    validation_record_fqdns=[
+        loadbalancer_alias_record.fqdn,
+        certificate.domain_validation_options[0].resource_record_name])
+
+# A Listener for a load balancer to listen HTTPS requests from the Internet
+listener = aws.lb.Listener("vftsLB-Listener",
+        load_balancer_arn=loadbalancer.load_balancer.arn,
+        port=443,
+        protocol="HTTPS",
+        ssl_policy="ELBSecurityPolicy-2016-08",
+        certificate_arn=certificate.arn,
+        default_actions=[aws.lb.ListenerDefaultActionArgs(
+            type="forward",
+            target_group_arn=loadbalancer.default_target_group.arn)],
+        opts=pulumi.ResourceOptions(depends_on=certificate_validation))
+
+# A certificate for a listener listening HTTPS requests from the Internet
+listener_certificate = aws.lb.ListenerCertificate("vftsListenerCert",
+    certificate_arn=certificate.arn,
+    listener_arn=listener.arn)
+
 pulumi.export("lb_vpcId", loadbalancer.vpc_id)
 pulumi.export("load_balancer", loadbalancer.load_balancer)
 
@@ -30,11 +86,11 @@ security_group = aws.ec2.SecurityGroup("vftsSG",
     name="vftsSG",
     vpc_id=vpc.vpc_id,
     ingress=[aws.ec2.SecurityGroupIngressArgs(
-        description="Allow inbound traffic from LB",
+        description="Allow HTTP inbound traffic from LB",
         from_port=80,
         to_port=80,
         protocol="tcp",
-        cidr_blocks=["10.0.0.0/16"],
+        cidr_blocks=[vpc.vpc.cidr_block],
     )],
     egress=[aws.ec2.SecurityGroupEgressArgs(
         description="Allow all outbound traffic",
@@ -79,5 +135,5 @@ service = awsx.ecs.FargateService("vftsService",
     ))
 pulumi.export("taskDefinition", service.task_definition)
 
-# The URL at which the container's HTTP endpoint will be available
-pulumi.export("url", pulumi.Output.concat("http://", loadbalancer.load_balancer.dns_name))
+# The URL at which the container's HTTPS endpoint will be available
+pulumi.export("url", pulumi.Output.concat("https://", site_domain_name))
