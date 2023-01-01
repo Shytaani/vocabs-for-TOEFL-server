57a5e4c76b0b65f0ebc804e6c5dff46ada7786b1
Shytaani <mambaout134@gmail.com>
2023-01-01 15:04:34 +0000

    Add scripts to update infra

---
+++ scripts/run-pulumi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
+set -e -x
+
+# Add the pulumi CLI to the PATH
+export PATH=$PATH:$HOME/.pulumi/bin
+
+pulumi stack select dev
+# The following is just a sample config setting that the hypothetical pulumi
+# program needs.
+# Learn more about pulumi configuration at: https://www.pulumi.com/docs/intro/concepts/config/
+pulumi config set aws:region ap-northeast-1
+# pulumi config set azure-native:location japaneast
+pulumi up --yes
