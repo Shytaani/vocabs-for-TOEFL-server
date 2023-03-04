4892abe041738c81fb4c080b0eaba9f3e66452e5
Tanishy <mambaout134@gmail.com>
2023-03-04 23:46:50 +0900

    Fix bug on initializing h2db

--- vfts/app/src/main/resources/data.sql
+++ vfts/app/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO WORD SELECT * FROM CSVREAD('data/word.csv');
-INSERT INTO DEFINITION SELECT * FROM CSVREAD('data/definition.csv');
-INSERT INTO SENTENCE SELECT * FROM CSVREAD('data/sentence.csv');
+INSERT INTO WORD SELECT * FROM CSVREAD('classpath:data/word.csv');
+INSERT INTO DEFINITION SELECT * FROM CSVREAD('classpath:data/definition.csv');
+INSERT INTO SENTENCE SELECT * FROM CSVREAD('classpath:data/sentence.csv');
